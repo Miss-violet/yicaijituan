4facbdce6165ed41236b5b40cae5f2ee6ad0e2b5
wuxi <1020375477@qq.com>
2018-07-13 17:05:10 +0800

    11

--- src/models/report.js
+++ src/models/report.js
@@ -1,6 +1,5 @@
 import pathToRegexp from 'path-to-regexp'
 import { info } from '../services/outbound'
-import { toDecimal } from '../utils/utils'
 
 export default {
   namespace: 'report',
@@ -29,11 +28,6 @@ export default {
     *info({ payload }, { call, put }) {
       const res = yield call(info, payload.id)
       if (res.code === 0) {
-        // res.data.standards.map(standardsItem => {
-        //   const { pointNum } = standardsItem
-        //   standardsItem.parameter = toDecimal(standardsItem.parameter, pointNum)
-        //   return standardsItem
-        // })
         yield put({
           type: 'success',
           payload: {
