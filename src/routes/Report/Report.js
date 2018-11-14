efd4f5fc0f661e4b557172bed4b642c496eda887
531587249@qq.com <531587249@qq.com>
2018-11-14 19:47:00 +0800

    二维码图片端口

--- src/routes/Report/Report.js
+++ src/routes/Report/Report.js
@@ -28,7 +28,7 @@ class Report extends Component {
             <p className={styles.name}>出厂合格证</p>
           </h1>
           <div className={styles.qrCode}>
-            <img src={`http://www.yicaijituan.cn:8091/${detail.qrcodeUrl}`} alt="qrCode" />
+            <img src={`http://www.yicaijituan.cn:8090/${detail.qrcodeUrl}`} alt="qrCode" />
           </div>
           <ul className={styles.leftList}>
             <li>
@@ -111,7 +111,7 @@ class Report extends Component {
               </Col>
             </Row>
             <div className={styles.qrCode}>
-              <img src={`http://www.yicaijituan.cn:8091/${detail.qrcodeUrl}`} alt="qrCode" />
+              <img src={`http://www.yicaijituan.cn:8090/${detail.qrcodeUrl}`} alt="qrCode" />
             </div>
           </div>
           <table className={table}>
