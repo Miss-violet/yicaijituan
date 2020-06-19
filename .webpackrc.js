d5091f71d38bfae5f1fb490dbb95728cc6099c75
Miss-violet <531587249@qq.com>
2020-06-19 08:30:30 +0800

    修改请求地址

--- .webpackrc.js
+++ .webpackrc.js
@@ -24,9 +24,9 @@ export default {
   hash: true,
   proxy: {
     '/api': {
-      target: 'http://www.yicaijituan.cn:8090/api/',
+      // target: 'http://www.yicaijituan.cn:8090/api/',
       // "target": "http://119.23.210.125:8090/api/",
-      // target: 'http://test.linhy.cn:8090/api/',
+      target: 'http://test.linhy.cn:8090/api/',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',
